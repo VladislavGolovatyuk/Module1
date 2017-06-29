555988f89b8d4fb4c43aa6725b7d7cb17e54d27e
VladislavGolovatuik <golovatuik.vladislav@gmail.com>
2017-06-29 01:04:36 -0700

    Перезалил дз правильно!

---
+++ src/module1/Summer.java
@@ -0,0 +1,11 @@
+package module1;
+
+/**
+ * Created by Cvladislav on 29.06.2017.
+ */
+public class Summer {
+    public static void main (String [] args){
+
+        System.out.println("Лето 2017 года пройдет в поисках истины))");
+    }
+}
